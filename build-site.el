ac9430bf4a9d4517b5bb70be8879dcb9305f5951
Leon Lan <l.lan@vu.nl>
2024-04-13 15:55:37 +0800

    Link to home page in navbar

--- build-site.el
+++ build-site.el
@@ -26,7 +26,7 @@
 
 ;; Add a navigation bar to the HTML output
 (setq org-html-preamble
-      "<nav> <h3> Leon Lan </h3> </nav>
+      "<nav> <h3> <a href=\"https://www.leonlan.com>\"> Leon Lan </a> </h3> </nav>
 ")
 
 ;; Define the publishing project
